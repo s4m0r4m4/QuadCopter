d83d608ac5e08f78fd2191cad19ef44bca75c0a1
s4m0r4m4 <schr0910@umn.edu>
2017-01-20 15:02:15 -0800

    Added function to print 4-elem array

--- readingSensors.ino
+++ readingSensors.ino
@@ -6,6 +6,12 @@ void serialPrintArray(float *vec){
   Serial.print(vec[1]); Serial.print("\t");
   Serial.print(vec[2]); Serial.print("\t");
 }
+void serialPrintArray4(float *vec){
+  Serial.print(vec[0]); Serial.print("\t");
+  Serial.print(vec[1]); Serial.print("\t");
+  Serial.print(vec[2]); Serial.print("\t");
+  Serial.print(vec[3]); Serial.print("\t");  
+}
 void serialPrintArrayLn(float *vec){
   serialPrintArray(vec); Serial.print("\n");
 }
